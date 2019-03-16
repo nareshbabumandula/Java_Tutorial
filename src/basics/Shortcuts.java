a9e258a8fe3111f281b6ae68108873276b0c456e
Mandula Naresh Babu <naresh2223@gmail.com>
2019-03-15 23:29:14 -0400

    Created Java Tutorial Project
    15-Mar-19 : Naresh : Checking-in Java_Tutorial project

---
+++ src/basics/Shortcuts.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Shortcuts 
+{
+
+	public static void main(String[] args) 
+	{
+		// Single line comment use //
+		System.out.println("Hello World"); 
+		// Print statement/method sysout+Ctrl+Spacebar
+		System.out.println("Welcome to Core Java");
+		// Block of code tp comment/uncomment - Ctrl+Shift+/ or Ctrl+Shift+\
+		/*int a=10;
+		int b=20;
+		int c=30;
+		String strName = "Ajay";*/
+	}
+
+}
