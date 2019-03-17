ea3367b6c12d6b6b5d7e63b82044c5f15c063291
Mandula Naresh Babu <naresh2223@gmail.com>
2019-03-16 23:53:51 -0400

    15-Mar-19: Naresh : Updated DataTypes.java class file with Widening and Narrowing examples

--- src/basics/DataTypes.java
+++ src/basics/DataTypes.java
@@ -10,7 +10,8 @@ public class DataTypes {
 		byte e =120;
 		boolean f = true;
 		boolean g=false;
-
+		long h=234324;
+		short i=1243;
 		System.out.println("Integer value of a is : " + a);
 		System.out.println("Float value of b is : " + b);
 		System.out.println("double value of c is : " + c);
@@ -18,9 +19,23 @@ public class DataTypes {
 		System.out.println("byte value of e is : " + e);
 		System.out.println("boolean value of f is : " + f);
 		System.out.println("boolean value of g is : " + g);
-
-
-
+		System.out.println("Long value of h is : "+ h);
+		System.out.println("Short value of i is : "+ i);
+		
+		//Data conversions - Widening and Narrowing
+		byte b1 = 124;
+		int c1 =b1;
+		System.out.println("Widening: Storing a smaller data type value (byte) into a bigger data type int : " + c1);
+		
+		int a2 = 145;
+		byte b2 =(byte) a2; //Type casting
+		System.out.println("Narrowing: Storing a bigger data type value (int) into a sammler data type byte : " + b2);
+		
+		
+		
+		
+		
+		
 	}
 
 }
